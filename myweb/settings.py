bd971dcb9bb9a3bbe32b2ac1f2ce5f11f2b39ed8
Anthony Luque <kasuru@hotmail.com>
2020-11-21 17:05:05 -0500

    [ADD] url static_files_storage

--- myweb/settings.py
+++ myweb/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
